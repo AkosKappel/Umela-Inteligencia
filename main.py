18637f84cf78d179c261dea709c6e7c3a7e270be
A-Kap-400 <kappelakos@gmail.com>
2020-10-13 22:08:17 +0200

    implemented Depth First Search

--- main.py
+++ main.py
@@ -6,83 +6,132 @@ def main():
     car_1 = Vehicle('red', 2, 2, 1, 'h')
     truck_1 = Vehicle('blue', 3, 1, 0, 'v')
     truck_2 = Vehicle('green', 3, 4, 3, 'h')
-    car_2 = Vehicle('yellow', 2, 0, 5, 'v')
+    car_2 = Vehicle('yellow', 2, 1, 5, 'v')
 
-    # while not puzzle.is_solved():  # TODO or car.can_move()
+    puzzle.show()
+    depth_first_search()
+    # while not puzzle.is_solved():
     #     puzzle.show()
     #     car_1.go_forward()
     # puzzle.show()
     # Testing
-    test_vehicle_movement()
-    test_save_and_load_state()
-    test_node_comparison()
+    # test_vehicle_movement()
+    # test_save_and_load_state()
+    # test_node_comparison()
 
 
-def test_vehicle_movement():
-    print(*Vehicle.all)
-    puzzle.show()
+def depth_first_search():
+    visited_nodes = set()
+    solution = None
+    total_steps = 0
 
-    print('B move 2 down')
-    Vehicle.all[1].go_forward(2)
-    print(*Vehicle.all)
-    puzzle.show()
+    start_state = puzzle.get_state()
+    start_state.create_children()
 
-    print('A move 2 right')
-    Vehicle.all[0].go_forward(2)
-    print(*Vehicle.all)
-    puzzle.show()
+    current = start_state
+    while True:
+        visited_nodes.add(current.state)
 
-    print('C move 1 left')
-    Vehicle.all[2].go_backward(1)
-    print(*Vehicle.all)
-    puzzle.show()
+        if puzzle.is_solved():
+            print('SOLVED')  # TODO remove
+            solution = current
+            break
 
-    print('B move 3 up')
-    Vehicle.all[1].go_backward(3)
-    print(*Vehicle.all)
-    puzzle.show()
+        while current.has_children():
+            child = current.children.pop()
+            if child not in visited_nodes:
+                current = Node(child, parent=current)
+                puzzle.set_state(current)
+                puzzle.show()
+                total_steps += 1
+                current.create_children()
+                break
 
+        if not current.has_children():
+            if current.has_parent():
+                current = current.parent
+            else:
+                print('UNSOLVABLE')  # TODO remove
+                break
 
-def test_save_and_load_state():
-    print('SAVE STATE 1')
-    state_1 = puzzle.get_state()
-    print(*Vehicle.all)
-    puzzle.show()
-
-    print('EMPTY GRID')
-    puzzle.empty_grid()
-    puzzle.show()
-
-    print('LOAD STATE 1')
-    puzzle.set_state(state_1)
-    print(*Vehicle.all)
-    puzzle.show()
+    if solution:
+        print(solution.state)
+    print('Total steps', total_steps)
 
 
-def test_node_comparison():
-    print('SAVED NODE 1')
-    node_1 = Node(puzzle.get_state())
-    puzzle.show()
-    print(node_1)
-
-    print('SAVED NODE 2')
-    Vehicle.all[1].go_forward(1)
-    node_2 = Node(puzzle.get_state())
-    puzzle.show()
-    print(node_2)
-
-    print('SAVED NODE 3')
-    Vehicle.all[1].go_backward(1)
-    node_3 = Node(puzzle.get_state())
-    puzzle.show()
-    print(node_3)
-
-    if node_1 == node_3:
-        print('NODE 1 == NODE 2')
-    if node_1 == node_2:
-        print('ERROR: NODE 1 != NODE 2', file=sys.stderr)
-    if node_3 != node_2:
-        print('NODE 3 != NODE 2')
+# def test_vehicle_movement():
+#     print(*Vehicle.all)
+#     puzzle.show()
+#
+#     print('B move 2 down')
+#     Vehicle.all[1].go_forward(2)
+#     print(*Vehicle.all)
+#     puzzle.show()
+#
+#     print('A move 2 right')
+#     Vehicle.all[0].go_forward(2)
+#     print(*Vehicle.all)
+#     puzzle.show()
+#
+#     print('C move 1 left')
+#     Vehicle.all[2].go_backward(1)
+#     print(*Vehicle.all)
+#     puzzle.show()
+#
+#     print('B move 3 up')
+#     Vehicle.all[1].go_backward(3)
+#     print(*Vehicle.all)
+#     puzzle.show()
+#
+#
+# def test_save_and_load_state():
+#     print('SAVE STATE 1')
+#     state_1 = puzzle.get_state()
+#     print(*Vehicle.all)
+#     puzzle.show()
+#
+#     print('EMPTY GRID')
+#     puzzle.empty_grid()
+#     puzzle.show()
+#
+#     print('LOAD STATE 1')
+#     puzzle.set_state(state_1)
+#     print(*Vehicle.all)
+#     puzzle.show()
+#
+#
+# def test_node_comparison():
+#     print('SAVED NODE 1')
+#     node_1 = puzzle.get_state()
+#     puzzle.show()
+#     print(node_1)
+#
+#     print('SAVED NODE 2')
+#     Vehicle.all[1].go_forward(1)
+#     node_2 = puzzle.get_state()
+#     puzzle.show()
+#     print(node_2)
+#
+#     print('SAVED NODE 3')
+#     Vehicle.all[1].go_backward(1)
+#     node_3 = puzzle.get_state()
+#     puzzle.show()
+#     print(node_3)
+#
+#     if node_1 == node_3:
+#         print('NODE 1 == NODE 2')
+#     if node_1 == node_2:
+#         print('ERROR: NODE 1 != NODE 2', file=sys.stderr)
+#     if node_3 != node_2:
+#         print('NODE 3 != NODE 2')
+#
+#     nodes = set()
+#     nodes.add(node_1.state)
+#     if node_2.state not in nodes:
+#         print('node 2 is not in nodes')
+#     nodes.add(node_2.state)
+#     if node_3.state in nodes:
+#         print('node 3 is in nodes')
 
 
 STYLE = {  # Available vehicle colors:
@@ -125,14 +174,14 @@ class Vehicle:
                 self.game.remove_vehicle(self)
                 self.y += n
                 self.game.place_vehicle(self)
-                return True
+                return self.game.get_state()
         else:
             if self.x + self.length + n <= self.game.size:  # right
                 self.game.remove_vehicle(self)
                 self.x += n
                 self.game.place_vehicle(self)
-                return True
-        return False
+                return self.game.get_state()
+        return None
 
     def go_backward(self, n=1):
         if self.is_vertical:
@@ -140,13 +189,29 @@ class Vehicle:
                 self.game.remove_vehicle(self)
                 self.y -= n
                 self.game.place_vehicle(self)
-                return True
+                return self.game.get_state()
         else:
             if self.x - n >= 0:  # left
                 self.game.remove_vehicle(self)
                 self.x -= n
                 self.game.place_vehicle(self)
-                return True
+                return self.game.get_state()
+        return None
+
+    def can_go_forward(self):
+        if self.is_vertical and self.y + self.length < self.game.size and \
+                self.game.is_empty(self.x, self.y + self.length):
+            return True
+        elif not self.is_vertical and self.x + self.length < self.game.size and \
+                self.game.is_empty(self.x + self.length, self.y):
+            return True
+        return False
+
+    def can_go_backward(self):
+        if self.is_vertical and self.y - 1 >= 0 and self.game.is_empty(self.x, self.y - 1):
+            return True
+        elif not self.is_vertical and self.x - 1 >= 0 and self.game.is_empty(self.x - 1, self.y):
+            return True
         return False
 
 
@@ -171,17 +236,17 @@ class RushHour:
             print('|', *self.grid[i], '|')
         print('-' * (self.size * 2 + 3))
 
-    def get_state(self):  # TODO return node ?
-        return tuple([(v.id, v.length, v.y, v.x, v.is_vertical) for v in self.vehicles])
+    def get_state(self):
+        return Node(tuple([(v.id, v.length, v.y, v.x, v.is_vertical) for v in self.vehicles]))
 
     def set_state(self, s):
         self.empty_grid()
         for i, v in enumerate(self.vehicles):
-            v.id, v.length, v.y, v.x, v.is_vertical = s[i]
+            v.id, v.length, v.y, v.x, v.is_vertical = s.state[i]
             self.place_vehicle(v)
 
     def place_vehicle(self, v):
-        if not self.is_empty(v.y, v.x, v.is_vertical, v.length):
+        if not self.is_empty(v.x, v.y, v.is_vertical, v.length):
             print(v, 'is overlapping an existing vehicle', file=sys.stderr)
             exit(1)
         self.set_block_of_vehicle(v, v.color + v.id + STYLE['END'] if v.color else v.id)
@@ -198,13 +263,13 @@ class RushHour:
                 self.grid[v.y][v.x + length] = cell_type
 
     def is_empty(self, x, y, is_vertical=None, length=1):
-        if is_vertical and x + length <= self.size:
-            return all(self.grid[x + i][y] == ' ' for i in range(length))
-        elif not is_vertical and y + length <= self.size:
-            return all(self.grid[x][y + i] == ' ' for i in range(length))
-        elif is_vertical is None and x < self.size and y < self.size:
-            return self.grid[x][y] == ' '
-        print(f'Vehicle starting at ({x}, {y}) is outside the grid', file=sys.stderr)
+        if is_vertical and y + length <= self.size:
+            return all(self.grid[y + i][x] == ' ' for i in range(length))
+        elif not is_vertical and x + length <= self.size:
+            return all(self.grid[y][x + i] == ' ' for i in range(length))
+        elif 0 <= x < self.size and 0 <= y < self.size:
+            return self.grid[y][x] == ' '
+        print(f'Point ({x}, {y}) is outside the grid', file=sys.stderr)
         exit(2)
 
     def empty_grid(self):
@@ -212,6 +277,7 @@ class RushHour:
             for j in range(self.size):
                 self.grid[i][j] = ' '
 
+    # @property
     def is_solved(self):
         v = self.vehicles[0]
         if v.is_vertical:
@@ -220,21 +286,34 @@ class RushHour:
 
 
 class Node:
-    all = []
-    # TODO add some kind of representation for all previous states
 
-    def __init__(self, game_state):
+    def __init__(self, game_state, parent=None):
         self.state = game_state
-        self.id = len(Node.all)
-        Node.all.append(self)
+        self.parent = parent
+        self.children = set()
 
     def __repr__(self):
-        return f'Node {self.id}: ' + \
+        return f'Node parent({self.parent}), children({self.children.__repr__()}):\n' + \
                ' '.join([f'({v[0]} {v[1]} {v[2]} {v[3]} ' +
                          ('v' if v[4] else 'h') + ')' for v in self.state])
 
     def __eq__(self, other):
         return self.state == other.state
+
+    def create_children(self):
+        temp_state = puzzle.get_state()
+        for v in Vehicle.all:
+            while v.can_go_forward():
+                self.children.add(v.go_forward().state)
+            while v.can_go_backward():
+                self.children.add(v.go_backward().state)
+            puzzle.set_state(temp_state)
+
+    def has_parent(self):
+        return self.parent
+
+    def has_children(self):
+        return self.children != set()
 
 
 puzzle = RushHour()
